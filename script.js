3e6756776be46e9f6cb0528b45185b92a635b0dc
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2025-03-22 19:28:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,39 +1,28 @@
-// Initial array
-const numbers = [1, 2, 3, 4];
+let div = document.querySelector("#output")
 
-// Function to create a promise that resolves after a delay
-function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+        const arr = [1,2,3,4]
 
-// Function to display results in the output div
-function displayResult(result) {
-    document.getElementById('output').textContent = result.join(', ');
-}
-
-// Initial Promise
-delay(3000).then(() => {
-    console.log('Initial Promise resolved with:', numbers);
-    return numbers;
-})
-// First Transformation: Filter out odd numbers
-.then(arr => {
-    const evenNumbers = arr.filter(num => num % 2 === 0);
-    return delay(1000).then(() => {
-        console.log('After filtering odd numbers:', evenNumbers);
-        displayResult(evenNumbers);
-        return evenNumbers;
-    });
-})
-// Second Transformation: Multiply even numbers by 2
-.then(evenNumbers => {
-    const multipliedNumbers = evenNumbers.map(num => num * 2);
-    return delay(2000).then(() => {
-        console.log('After multiplying by 2:', multipliedNumbers);
-        displayResult(multipliedNumbers);
-        return multipliedNumbers;
-    });
-})
-.catch(err => {
-    console.error('An error occurred:', err);
-});
+        new Promise(function(res,rej){
+            let newArr = []
+            for(let t of arr){
+                if(t%2===0){
+                    newArr.push(t)
+                }
+            }
+            setTimeout(()=>{
+                res(newArr)
+            },1000)
+        })
+        .then((data)=>{
+            div.innerText = data;
+            let array = []
+            for(let t of data){
+                array.push(t*2)
+            }
+            return array;
+        })
+        .then((data)=>{
+            setTimeout(()=>{
+                div.innerText = data
+            },3000)
+        })
